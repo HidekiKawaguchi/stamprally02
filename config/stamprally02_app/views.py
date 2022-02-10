bb0e04d7093c3dbf41981b3670f21bcc8bf5cc3e
johnpori <h.kawaguchi0610@gmail.com>
2022-02-10 15:35:01 +0900

    CSSファイル入れ込みました。HTMLファイルに入れ込みました。

--- config/stamprally02_app/views.py
+++ config/stamprally02_app/views.py
@@ -14,5 +14,7 @@ from . import views
 
 # View関数を任意に定義
 def index(request):
+    # 変数設定
+    params = {"message_me": "Hello World"}
     # 出力
-    return render(request, 'App_Folder_HTML/index.html')
+    return render(request, 'App_Folder_HTML/index.html', context=params)
